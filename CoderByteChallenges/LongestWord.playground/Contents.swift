16da62ac332c687727727d88ef28c2f263c2ee3f
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-01-04 11:59:42 +0200

    Longest Word Challenge - Finished - Commit Version 1

--- CoderByteChallenges/LongestWord.playground/Contents.swift
+++ CoderByteChallenges/LongestWord.playground/Contents.swift
@@ -1,15 +1,41 @@
 import UIKit
 
-func CodelandUsernameValidation(_ str: String) -> String {
+//Longest Word
+//
+//Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
+//Examples
+//
+//Input: "fun&!! time"
+//Output: time
+//Input: "I love dogs"
+//Output: love
 
-  // code goes here
-  // Note: feel free to modify the return type of this function
-  return str
 
+func LongestWord(_ sen: String) -> String {
+    
+    // code goes here
+    // Note: feel free to modify the return type of this function
+    
+    let words = sen.split{$0 == " "}.map(String.init)
+    
+    var maxLenght = 0
+    var longestWord = ""
+    
+    words.forEach {
+        let word = $0.filter { !"!%?&%.,".contains($0)}
+        if word.count > maxLenght {
+            maxLenght = word.count
+            longestWord = word
+        }
+    }
+    
+    return longestWord
+    
 }
 
 // keep this function call here
-print(CodelandUsernameValidation(readLine(strippingNewline: true) ?? ""));
+print(LongestWord("fun&!! time"));
+print(LongestWord("I love dogs"));
 
 
 
