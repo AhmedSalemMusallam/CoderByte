091383c4fe4df9e7d1318815991380fa6d0d0a44
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-01-04 12:17:36 +0200

    First Reverse Challenge - Finished - Commit Version 1

--- CoderByteChallenges/FirstReverse.playground/Contents.swift
+++ CoderByteChallenges/FirstReverse.playground/Contents.swift
@@ -1,16 +1,26 @@
 import UIKit
 
-func CodelandUsernameValidation(_ str: String) -> String {
+//First Reverse
+//
+//Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+//Examples
+//
+//Input: "coderbyte"
+//Output: etybredoc
+//Input: "I Love Code"
+//Output: edoC evoL I
+
+func FirstReverse(_ str: String) -> String {
 
   // code goes here
   // Note: feel free to modify the return type of this function
-  return str
+  return String(str.reversed())
 
 }
 
 // keep this function call here
-print(CodelandUsernameValidation(readLine(strippingNewline: true) ?? ""));
-
+print(FirstReverse("coderbyte"));
+print(FirstReverse("I Love Code"));
 
 
 
